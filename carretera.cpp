56af8b727c13f5ceb9fcaebcc5be79d789ffd125
BryanOlivares <49683706+BryanOlivares@users.noreply.github.com>
2019-04-29 20:27:35 -0500

    Add files via upload

---
+++ carretera.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int d=360;
+	int d1=70;
+	int d2=150;
+	int e;
+	cout<<"CARRETERA QUITO-ATACAMES"<<endl;
+	cout<<"LA DISTACIA ENTRE LAS DOS CIUDADES ES = "<<d<<endl;
+	cout<<"RECORRIDO QUITO-ATACAMES SE ENCUENTRA EN EL KILOMETRO 70"<<endl;
+	cout<<"RECORRIDO ATACAMES-QUITO SE ENCUENTRA EN EL KILOMETRO 150"<<endl;
+	e=d-(d1+d2);
+	cout<<"SE ENCUENTRAN EN EL KILOMETRO "<<e<<" CON RESPECTO A QUITO-ATACAMES"<<endl;
+	cout<<"SE ENCUENTRAN EN EL KILOMETRO "<<(d1+d2)<<" CON RESPECTO A ATACAMES-QUITO"<<endl;
+	
+	
+}
